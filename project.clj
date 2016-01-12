007325976483c4eed69ba081959aebcdf1276f8e
Duncan McGreggor <oubiwann@gmail.com>
2016-01-12 17:21:08 -0600

    Updated versions.

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject clojureinterpreter "0.1.0-SNAPSHOT"
+(defproject clojureinterpreter "0.2.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.apache.zeppelin/zeppelin-interpreter "0.5.0-SNAPSHOT"]
-                 [org.clojure/tools.nrepl "0.2.10"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.apache.zeppelin/zeppelin-interpreter "0.5.0-incubating"]
+                 [org.clojure/tools.nrepl "0.2.11"]]
   :prep-tasks [["compile" "clojureinterpreter.core"]
                "javac"
                "compile"]
