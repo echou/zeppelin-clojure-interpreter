705d271305534f3ef48fbb28a4c4e7b7ff9e73bf
Duncan McGreggor <oubiwann@gmail.com>
2016-01-12 22:49:38 -0600

    Updated metadata.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clojureinterpreter "0.2.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A Clojure Interpreter for Zeppelin Notebooks"
+  :url "https://github.com/bfabry/zeppelin-clojure-interpreter"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
