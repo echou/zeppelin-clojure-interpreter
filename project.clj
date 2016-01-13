e2ed05ec103553ff8ce48ffb919e489330103c6e
Duncan McGreggor <oubiwann@gmail.com>
2016-01-13 11:08:56 -0600

    Added graphing library.

--- project.clj
+++ project.clj
@@ -8,7 +8,8 @@
                  [org.clojure/tools.nrepl "0.2.11"]
                  [ch.qos.logback/logback-classic "1.1.3"]
                  [yieldbot/flambo "0.7.1"]
-                 [gorillalabs/sparkling "1.2.3"]]
+                 [gorillalabs/sparkling "1.2.3"]
+                 [org.apache.spark/spark-graphx_2.10 "1.6.0"]]
   :resource-paths ["resources/zeppelin-interpreter-0.5.5-incubating.jar"]
   :prep-tasks [["compile" "clojureinterpreter.core"]
                "javac"
