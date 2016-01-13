731988ff8ead98b8bb8ac288d6eef714c623962e
Duncan McGreggor <oubiwann@gmail.com>
2016-01-13 09:44:09 -0600

    Added two popular Clojure spark libraries.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojureinterpreter "0.2.0"
+(defproject clojureinterpreter "0.2.1"
   :description "A Clojure Interpreter for Zeppelin Notebooks"
   :url "https://github.com/bfabry/zeppelin-clojure-interpreter"
   :license {:name "Eclipse Public License"
@@ -6,7 +6,9 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/tools.logging "0.3.1"]
                  [org.clojure/tools.nrepl "0.2.11"]
-                 [ch.qos.logback/logback-classic "1.1.3"]]
+                 [ch.qos.logback/logback-classic "1.1.3"]
+                 [yieldbot/flambo "0.7.1"]
+                 [gorillalabs/sparkling "1.2.3"]]
   :resource-paths ["resources/zeppelin-interpreter-0.5.5-incubating.jar"]
   :prep-tasks [["compile" "clojureinterpreter.core"]
                "javac"
